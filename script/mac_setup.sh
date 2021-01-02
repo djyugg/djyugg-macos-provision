d9d2a3abe4584b429c1415668175eb9fbbc6f7fb
djyugg <9217500+djyugg@users.noreply.github.com>
2021-01-02 10:13:54 +0900

    menu barの設定を追加

--- script/mac_setup.sh
+++ script/mac_setup.sh
@@ -23,3 +23,15 @@ defaults write com.apple.finder ShowPathbar -bool true
 
 ## Finder 設定反映
 killall Finder
+
+# Menu bar
+defaults write com.apple.systemuiserver menuExtras -array \
+"/System/Library/CoreServices/Menu Extras/Clock.menu" \
+"/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+"/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
+"/System/Library/CoreServices/Menu Extras/VPN.menu" \
+"/System/Library/CoreServices/Menu Extras/Volume.menu" \
+"/System/Library/CoreServices/Menu Extras/Displays.menu"
+
+## Menu barの変更反映
+killall -kill SystemUIServer
